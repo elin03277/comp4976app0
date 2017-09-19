8755876f89fe647beabbd79a542a4d6613c426a8
Eric Lin <elin03277@gmail.com>
2017-09-19 11:19:45 -0700

    Lab 2

---
+++ HappyMVC/Controllers/FilesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace HappyMVC.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            string[] files = Directory.GetFiles(Server.MapPath("~/TextFiles"));
+            ViewBag.fileArray = files;
+
+            return View();
+        }
+
+        public ActionResult Content(string id)
+        {
+            var pathName = Server.MapPath($"~/TextFiles/{id}.txt");
+            ViewBag.content = System.IO.File.ReadAllText(pathName);
+
+            return View();
+        }
+    }
+}
